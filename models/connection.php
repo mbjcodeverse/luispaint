51bd3c35fbac3a1e915a1a4ec4748b162e67d690
mbjcodeverse <markhjolive@gmail.com>
2025-04-04 03:31:32 +0800

    data connect online

--- models/connection.php
+++ models/connection.php
@@ -2,9 +2,9 @@
 
 class Connection{
 	public function connect(){
-		$link = new PDO("mysql:host=localhost;dbname=luispaint", "root", "");
+		// $link = new PDO("mysql:host=localhost;dbname=luispaint", "root", "");
 
-		// $link = new PDO("mysql:host=localhost;dbname=u661302059_luispaint", "u661302059_luispaint", "BcdLuisPa1nt!");
+		$link = new PDO("mysql:host=localhost;dbname=u661302059_luispaint", "u661302059_luispaint", "BcdLuisPa1nt!");
 
 		$link -> exec("set names utf8");
 		return $link;
