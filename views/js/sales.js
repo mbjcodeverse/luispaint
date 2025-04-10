4d47efa6fb3df918e7a4e1f9239d777e8472095a
mbjcodeverse <markhjolive@gmail.com>
2025-04-11 00:33:08 +0800

    test start date range - 2010-01-01

--- views/js/sales.js
+++ views/js/sales.js
@@ -309,6 +309,7 @@ $(function() {
     });
 
     $("#lbl-lst-date-range").click(function(){
+        alert("mom");
         $('#lst_date_range').data('daterangepicker').setStartDate(moment('2010-01-01'));
         $('#lst_date_range').data('daterangepicker').setEndDate(moment());
         
