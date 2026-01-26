ce25d368d3f7afee67b4a36e3022e47b76ddcb30
mbjcodeverse <markhjolive@gmail.com>
2026-01-26 16:31:58 +0800

    connection to host

--- models/connection.php
+++ models/connection.php
@@ -2,9 +2,9 @@
 
 class Connection{
 	public function connect(){
-		$link = new PDO("mysql:host=localhost;dbname=trilab", "root", "");
+		// $link = new PDO("mysql:host=localhost;dbname=trilab", "root", "");
 
-		// $link = new PDO("mysql:host=localhost;dbname=u911784584_trilab", "u911784584_trilab", "TriLab2026onwards");
+		$link = new PDO("mysql:host=localhost;dbname=u911784584_trilab", "u911784584_trilab", "TriLab2026onwards");
 
 		$link -> exec("set names utf8");
 		return $link;
